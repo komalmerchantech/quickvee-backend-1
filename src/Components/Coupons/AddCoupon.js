9a878dfc9f802550c5a4353d870f3189d3793226
QuickveeAdmin <priya@merchantech.com>
2023-12-26 11:21:49 +0530

    coupon-page

--- src/Components/Coupons/AddCoupon.js
+++ src/Components/Coupons/AddCoupon.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import AddNewCategory from "../../Assests/Dashboard/Left.svg";
 
-const AddCoupon = () => {
+const AddCoupon = ({ seVisible }) => {
+  const [couponStates, setCouponStates] = useState();
+
+  const handleCheckboxChange = (couponName) => (e) => {
+    setCouponStates({
+      ...couponStates,
+      [couponName]: e.target.checked,
+    });
+  };
   return (
-    <div>AddCoupon</div>
-  )
-}
+    <>
+      <div className="mt-10">
+        <div className="q-add-categories-section">
+          <div className="q-add-categories-section-header">
+            <span onClick={() => seVisible("CouponDiscount")}>
+              <img
+                src={AddNewCategory}
+                alt="Add-New-Category"
+                className="h-9 w-9"
+              />
+              <span>Add Coupons</span>
+            </span>
+          </div>
+          <div className="q-add-categories-section-middle-form">
+            <div className="q_coupon_Add_status_btn">
+              <p>show online</p>
+              <p>
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    checked={{ couponStates }}
+                    onChange={handleCheckboxChange("")}
+                  />
+                  <span className="slider round"></span>
+                </label>
+              </p>
+            </div>
+            <div className="q-add-coupon-single-input">
+              <label for="coupon">Coupon Code</label>
+              <input type="text" id="coupon" name="coupon_code" />
+            </div>
 
-export default AddCoupon
+            <div className="q-add-coupon-single-input">
+              <label for="description">Description</label>
+              <textarea
+                id="description"
+                name="description"
+                rows="4"
+                cols="50"
+              ></textarea>
+            </div>
+
+            <div className="q_coupon_calculation_amount">
+              <label for="minimumorder">Minimum Order Amount</label>
+              <input type="text" id="order" name="minium_order" />
+            </div>
+            <div className="q_coupon_calculation_amount">
+              <label for="minimumorder">Discount</label>
+              <input type="text" id="discount" name="discount" />
+            </div>
+          </div>
+          <div className="q-add-categories-section-middle-footer">
+            <button className="quic-btn quic-btn-save">Add</button>
+            <button
+              onClick={() => seVisible("CouponDiscount")}
+              className="quic-btn quic-btn-cancle"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddCoupon;
--- src/Components/Coupons/CouponDiscount.js
+++ src/Components/Coupons/CouponDiscount.js
@@ -5,92 +5,188 @@ import DeletIcon from "../../Assests/Dashboard/delete.svg";
 import Edit from "../../Assests/Dashboard/edit.svg";
 
 const CouponDiscount = ({ seVisible }) => {
-  const [coupons, setCoupons] = useState([
-    {
-      id: 1,
-      code: "FLAT20",
-      discount: "20% OFF on a minimum order of $30.00",
-      validDate: "1/08/2022 - 12:00AM to 1/09/2022 - 12:00AM",
-      maxDiscount: "$50",
-      status: "Enabled",
-    },
-    // Add more coupons as needed
-  ]);
+  const [isChecked, setIsChecked] = useState(true);
+  const [couponStates, setCouponStates] = useState({
+    coupon1: true,
+    coupon2: false,
+    coupon3: false,
+    coupon4: false,
+    // ... and so on for each coupon
+  });
 
-  const handleEdit = (id) => {
-    // Add logic to handle edit
-    console.log(`Edit coupon with ID ${id}`);
+  const handleCheckboxChange = (couponName) => (e) => {
+    setCouponStates({
+      ...couponStates,
+      [couponName]: e.target.checked,
+    });
   };
-
-  const handleDelete = (id) => {
-    // Add logic to handle delete
-    setCoupons((prevCoupons) =>
-      prevCoupons.filter((coupon) => coupon.id !== id)
-    );
-    console.log(`Delete coupon with ID ${id}`);
-  };
-
-  const handleToggleStatus = (id) => {
-    setCoupons((prevCoupons) =>
-      prevCoupons.map((coupon) =>
-        coupon.id === id
-          ? {
-              ...coupon,
-              status: coupon.status === "Enabled" ? "Disabled" : "Enabled",
-            }
-          : coupon
-      )
-    );
-  };
-
   return (
     <>
       <div className="mt-10">
-        <div className="q-attributes-bottom-detail-section">
-          <div className="q-attributes-bottom-header">
+        <div className="q-coupon-bottom-detail-section">
+          <div className="q-coupon-bottom-header">
             <span>Coupon</span>
 
             <p onClick={() => seVisible("AddCoupon")}>
               Add New Coupon <img src={AddIcon} alt="add-icon" />
             </p>
           </div>
-          <div className="flex justify-between w-full gap-8">
-            <div className="q_copuon_header mx-12">
+          <div className="q_main_section_coupon">
+            <div
+              className={`q_copuon_header mx-6 ${couponStates.coupon1 ? "active" : ""
+                }`}
+            >
               <div className="flex justify-between w-full">
                 <div className="q_coupon_code">
                   <p>FLAT20</p>
                 </div>
                 <div className="flex space-x-2 p-4">
-                  <img src={Edit} alt="" className="h-12 w-16" />
-
-                  <img src={DeletIcon} alt="delet" className="h-12 w-16" />
+                  <img src={Edit} alt="" className="h-8 w-8" />
+                  <img src={DeletIcon} alt="delet" className="h-8 w-8" />
                 </div>
               </div>
-
               <div className="q_discount_coupon_Code">
                 <div className="">20% OFF on minimum order of $30.00</div>
               </div>
-              <div className="">
-                
+              <div className="q_coupon_deatails_validtimes">
+                <p>Valid from</p>
+                <p className="q_date_details">1/08/2022 - 12:00AM to 1/09/2022 - 12:00AM</p>
+              </div>
+              <div className="q_coupon_discountCode">
+                <p>Max Discount</p>
+                <p className="q_date_details">$6.00</p>
+              </div>
+              <div className="q_coupon_status_btn">
+                <p>show online</p>
+                <p>
+                  <label className="switch">
+                    <input
+                      type="checkbox"
+                      checked={couponStates.coupon1}
+                      onChange={handleCheckboxChange("coupon1")}
+                    />
+                    <span className="slider round"></span>
+                  </label>
+                </p>
               </div>
             </div>
-
-            <div className="q_copuon_header mx-4">
+            <div
+              className={`q_copuon_header mx-6 ${couponStates.coupon2 ? "active" : ""
+                }`}
+            >
+              <div className="flex justify-between w-full">
+                <div className="q_coupon_code">
+                  <p>FLAT20</p>
+                </div>
+                <div className="flex space-x-2 p-4">
+                  <img src={Edit} alt="" className="h-8 w-8" />
+                  <img src={DeletIcon} alt="delet" className="h-8 w-8" />
+                </div>
+              </div>
+              <div className="q_discount_coupon_Code">
+                <div className="">20% OFF on minimum order of $30.00</div>
+              </div>
+              <div className="q_coupon_deatails_validtimes">
+                <p>Valid from</p>
+                <p className="q_date_details">1/08/2022 - 12:00AM to 1/09/2022 - 12:00AM</p>
+              </div>
+              <div className="q_coupon_discountCode">
+                <p>Max Discount</p>
+                <p className="q_date_details">$6.00</p>
+              </div>
+              <div className="q_coupon_status_btn">
+                <p>show online</p>
+                <p>
+                  <label className="switch">
+                    <input
+                      type="checkbox"
+                      checked={couponStates.coupon2}
+                      onChange={handleCheckboxChange("coupon2")}
+                    />
+                    <span className="slider round"></span>
+                  </label>
+                </p>
+              </div>
+            </div>
+            <div
+              className={`q_copuon_header mx-6 ${couponStates.coupon3 ? "active" : ""
+                }`}
+            >
               <div className="flex justify-between w-full">
                 <div className="q_coupon_code">
                   <p>SUMMER25</p>
                 </div>
                 <div className="flex space-x-2 p-4">
-                  <img src={Edit} alt="" className="h-12 w-16" />
-
-                  <img src={DeletIcon} alt="delet" className="h-12 w-16" />
+                  <img src={Edit} alt="" className="h-8 w-8" />
+                  <img src={DeletIcon} alt="delet" className="h-8 w-8" />
                 </div>
               </div>
               <div className="q_discount_coupon_Code">
                 <div className="">20% OFF on minimum order of $30.00</div>
               </div>
+              <div className="q_coupon_deatails_validtimes">
+                <p>Valid from</p>
+                <p className="q_date_details">1/08/2022 - 12:00AM to 1/09/2022 - 12:00AM</p>
+              </div>
+              <div className="q_coupon_discountCode">
+                <p>Max Discount</p>
+                <p className="q_date_details">$6.00</p>
+              </div>
+              <div className="q_coupon_status_btn">
+                <p>show online</p>
+                <p>
+                  <label className="switch">
+                    <input
+                      type="checkbox"
+                      checked={couponStates.coupon3}
+                      onChange={handleCheckboxChange("coupon3")}
+                    />
+                    <span className="slider round"></span>
+                  </label>
+                </p>
+              </div>
             </div>
+            <div
+              className={`q_copuon_header mx-6 ${couponStates.coupon4 ? "active" : ""
+                }`}
+            >
+              <div className="flex justify-between w-full">
+                <div className="q_coupon_code">
+                  <p>FLAT20</p>
+                </div>
+                <div className="flex space-x-2 p-4">
+                  <img src={Edit} alt="" className="h-8 w-8" />
+                  <img src={DeletIcon} alt="delet" className="h-8 w-8" />
+                </div>
+              </div>
+              <div className="q_discount_coupon_Code">
+                <div className="">20% OFF on minimum order of $30.00</div>
+              </div>
+              <div className="q_coupon_deatails_validtimes">
+                <p>Valid from</p>
+                <p className="q_date_details">1/08/2022 - 12:00AM to 1/09/2022 - 12:00AM</p>
+              </div>
+              <div className="q_coupon_discountCode">
+                <p>Max Discount</p>
+                <p className="q_date_details">$6.00</p>
+              </div>
+              <div className="q_coupon_status_btn">
+                <p>show online</p>
+                <p>
+                  <label className="switch">
+                    <input
+                      type="checkbox"
+                      checked={couponStates.coupon4}
+                      onChange={handleCheckboxChange("coupon4")}
+                    />
+                    <span className="slider round"></span>
+                  </label>
+                </p>
+              </div>
+            </div>
+
           </div>
+
         </div>
       </div>
     </>
