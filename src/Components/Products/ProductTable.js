478785e698451abb96f08ff7114eddd749e9914c
Sachin <sachin@imerchantech.com>
2024-04-04 11:03:50 +0530

    add product start and prouctTable commit

--- src/Components/Products/ProductTable.js
+++ src/Components/Products/ProductTable.js
@@ -10,16 +10,12 @@ import { BASE_URL } from "../../Constants/Config";
 import InfiniteScroll from "react-infinite-scroll-component";
 import ProductRow from "./ProductRow";
 
-const ProductTable = ({selectedListingType,selectedListingTypeValue,productsList,setproductsList,categoryId,selectedStatus,selectedStatusValue}) => 
-{
+const ProductTable = ({ selectedListingType, selectedListingTypeValue, productsList, setproductsList, categoryId, selectedStatus, selectedStatusValue }) => {
   let listing_type = 0;
   const ProductsListDataState = useSelector((state) => state.productsListData);
-  const { hasMore,offset,limit } = useSelector((state) => state.productsListData);
-  
-  // useEffect(()=> {
-  //   console.log(categoryId+'asdasd')
-  //   console.log('product tables');
-  // });
+  const { hasMore, offset, limit } = useSelector((state) => state.productsListData);
+
+
   useEffect(() => {
     if (
       !ProductsListDataState.loading &&
@@ -76,7 +72,7 @@ const ProductTable = ({selectedListingType,selectedListingTypeValue,productsList
     console.log(event.target.id);
     console.log(event.target.name);
   }
-  
+
   const [items, setItems] = useState(Array.from({ length: 10 }));
   const style = {
     height: 30,
@@ -86,37 +82,34 @@ const ProductTable = ({selectedListingType,selectedListingTypeValue,productsList
   };
   const fetchMoreData = () => {
     let page = 0;
-    if(productsList.length > 0 )
-    {
-       page = productsList.length / 10 ;
+    if (productsList.length > 0) {
+      page = productsList.length / 10;
     }
-    
-    if(selectedListingType == 'Variant listing'){
-       listing_type = 1;
-    }else{
+
+    if (selectedListingType == 'Variant listing') {
+      listing_type = 1;
+    } else {
       listing_type = 0;
     }
     //let page = productsList.length / 10 + 1 ;
-    
-    console.log(page+'page');
+
+    console.log(page + 'page');
     console.log(productsList);
     let data1 = {
       merchant_id: "MAL0100CA",
-      format:"json",
+      format: "json",
       category_id: categoryId,
       show_status: selectedStatusValue,
       listing_type: selectedListingTypeValue,
       offset: offset,
       limit: 10,
-      page : page
+      page: page
     };
     if (data1) {
       dispatch(fetchProductsData(data1));
-      // console.log(productsList);
+     
     }
-    // setTimeout(() => {
-    //   setproductsList(productsList.concat(ProductsListDataState.productsData));
-    // }, 150);
+  
     setTimeout(() => {
       setItems(items.concat(Array.from({ length: 15 })));
     }, 150);
@@ -125,14 +118,15 @@ const ProductTable = ({selectedListingType,selectedListingTypeValue,productsList
 
   return (
     <>
+    <div className="box">
       <div className="q-category-bottom-detail-section" id='123'>
         <div className="">
           <div className="q-category-bottom-header">
             <span>Products</span>
             <p className="">
-              <Link to="/product-edit">
+              
                 Add New Product
-              </Link>
+             
               <Link to="/product-add">
                 <img src={AddIcon} alt="add-icon" />
               </Link>
@@ -140,7 +134,7 @@ const ProductTable = ({selectedListingType,selectedListingTypeValue,productsList
           </div>
           <div className="q-category-bottom-detail-section">
             <div className="q-category-bottom-header-sticky">
-              <div className="q-category-bottom-categories-header" style={{ position: "sticky" , top : "0px" }} >
+              <div className="q-category-bottom-categories-header" style={{ position: "sticky", top: "0px" }} >
                 <p className="categories-sort">Sort</p>
                 <p className="categories-sort"></p>
                 <p className="categories-title">Title</p>
@@ -156,87 +150,32 @@ const ProductTable = ({selectedListingType,selectedListingTypeValue,productsList
                 <p className="categories-items" >Delete</p>
                 <p className=""></p>
               </div>
-          
-              <div  id="scrollableDiv" style={{ height: 300 , overflow: "auto" }}>
-                <InfiniteScroll 
+
+              <div id="scrollableDiv" style={{ height: 300, overflow: "auto" }}>
+                <InfiniteScroll
                   dataLength={productsList.length}
                   next={fetchMoreData}
                   hasMore={hasMore}
                   loader={<h4>Loading...</h4>}
-                  scrollableTarget="scrollableDiv" 
-                  endMessage = {<h3>ALL products have been listed above</h3>}
-                  >
+                  scrollableTarget="scrollableDiv"
+                  endMessage={<h3>ALL products have been listed above</h3>}
+                >
                   {
-                  productsList?.length >= 1 &&
-                  productsList.map((product, index) => (
-                  
-                  // add function in below object 
-                  <ProductRow key={index} {...{ Avail_Online, index, product, checkStatus, handleError }} />
+                    productsList?.length >= 1 &&
+                    productsList.map((product, index) => (
 
-                // <div key={index} className="q-attributes-bottom-attriButes-single-attributes">
-                //   <p className="categories-sort"><img src={SortIcon} alt="" className="" /></p>
-                //   <p className="categories-title">{product.title}</p>
-                //   <p className="categories-sort"></p>
-                //   <p className="categories-title">{product.category_name}</p>
-                //   <p className="categories-sort"></p>
-                  
-                //   <div className="categories-title">
-                //   <div className="flex flex-wrap gap-3 ">
-                //     <label className="q_resigter_setting_section" style={{color:"#000",fontSize:"18px"}}>Delivery
-                //       <input type="checkbox"  
-                //               id={"delivery_check"+product.id}
-                //               name="delivery_check"
-                //               checked= {(product.show_type == 0 || product.show_type == 2) ? true : false}
-                //               value="2"
-                //               onChange={(event) => {Avail_Online(event);}} 
-                //       />
-                //       <span className="checkmark"></span></label>
-                //     <label className="q_resigter_setting_section" style={{color:"#000",fontSize:"18px"}}>Pickup
-                //       <input type="checkbox"
-                //             id={"pickup_check"+product.id}
-                //             name="pickup_check"
-                //             checked= {(product.show_type == 0 || product.show_type == 1) ? true : false}
-                //             value="1"
-                //             onChange={(event) => {Avail_Online(event);}} 
-                //       />
-                //       <span className="checkmark"></span></label>
-                //   </div>
-                //   </div>
-                //   <p className="categories-sort"></p>
-                //   <p className="categories-title">{checkStatus(product.show_status)}    </p>
-                //   <p className="categories-sort"></p>
-                //   <div className="categories-items" style={{width:"50%"}}>
+                     
+                      <ProductRow key={index} {...{ Avail_Online, index, product, checkStatus, handleError }} />
 
-
-                //   <div className="flex items-center space-x-2 text-base">
-        
-                //   </div>
-                //   <div className="mt-3 flex -space-x-2 overflow-hidden">
-                //     { 
-                //     product?.media?.split(",").slice(0, 4).map((item, index) => (
-                        
-                //         <img key={index} className="inline-block h-12 w-12 rounded-full ring-2 ring-white" src={BASE_URL+"upload/products/MAL0100CA/"+item} onError={handleError}  alt=""/>
-                //       ))}
-                //   </div>
-                //   {( product?.media?.split(",").length>4) ? (
-                //       <div className="mt-3 text-sm font-medium">
-                //         <a href="#" className="text-blue-500">+ {product.media.split(",").length - 4 } others</a>
-                //       </div>
-                //     ): ''
-                //   }
-                  
-                  
-
-                //   </div>
-                //   <p className=""><img src={DeleteIcon} alt=" " className="w-16 h-16" /></p>
-                // </div>
-                )
-                )}
+                     
+                    )
+                    )}
                 </InfiniteScroll>
               </div>
             </div>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
